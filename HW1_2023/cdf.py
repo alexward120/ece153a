d9bf06105583b6b1a26806031dd04afa1234b637
Alexander Ward <alexanderward@ucsb.edu>
2023-10-10 12:33:30 -0700

    added fraction of times below worst case time

--- HW1_2023/cdf.py
+++ HW1_2023/cdf.py
@@ -43,6 +43,19 @@ cdf_disabled_cache = np.arange(1, total_data_points + 1) / total_data_points
 total_data_points = len(access_times_enabled_cache)
 cdf_enabled_cache = np.arange(1, total_data_points + 1) / total_data_points
 
+worst_case_access_time_disabled_cache = access_times_disabled_cache[-1]
+worst_case_access_time_enabled_cache = access_times_enabled_cache[-1]
+
+# Estimate the fraction of time below the worst-case time
+fraction_below_worst_case_disabled_cache = cdf_disabled_cache[access_times_disabled_cache.index(worst_case_access_time_disabled_cache)]
+fraction_below_worst_case_enabled_cache = cdf_enabled_cache[access_times_enabled_cache.index(worst_case_access_time_enabled_cache)]
+
+print(f"Fraction of time below worst-case time (Disabled Cache): {fraction_below_worst_case_disabled_cache:.2f}")
+print(f"Fraction of time below worst-case time (Enabled Cache): {fraction_below_worst_case_enabled_cache:.2f}")
+
+print(cdf_enabled_cache)
+print(cdf_disabled_cache)
+
 # Plot the CDF
 plt.plot(access_times_disabled_cache, cdf_disabled_cache, label="Disabled Cache")
 plt.plot(access_times_enabled_cache, cdf_enabled_cache, label="Enabled Cache")

773d38c65a788d6c5769c470a0f319c74e41224e
alexward120 <alexanderward@ucsb.edu>
2023-11-06 20:14:38 -0800

    lab2b

---
+++ Labs/HardwareSetup/lab2b/lab2b.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_lab2b.elf_on_local.tcl
@@ -0,0 +1,9 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys A7 -100T 210292B62099A"} -index 0
+rst -system
+after 3000
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys A7 -100T 210292B62099A"} -index 0
+dow C:/Users/zande/Documents/VSCODE/SchoolStuff/ece153a/Labs/HardwareSetup/lab2b/lab2b.sdk/lab2b/Debug/lab2b.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys A7 -100T 210292B62099A"} -index 0
+con
